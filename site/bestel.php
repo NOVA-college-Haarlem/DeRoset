e3712ccfa1646bde559c170a6a784e6987deee81
azucardrain <186533@novacollege.nl>
2023-03-31 11:36:30 +0200

    asdasd

--- site/bestel.php
+++ site/bestel.php
@@ -1,20 +1,19 @@
 <?php include 'nav.php';
 require 'database.php';
-
+include 'cart.php';
 $stmt = $conn->prepare("SELECT * FROM ice_cream");
 $stmt->execute();
 $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 $flavor = $stmt->fetchAll();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="store.js" async></script>
     <title>bestel</title>
 </head>
@@ -32,12 +31,18 @@ $flavor = $stmt->fetchAll();
                     <strong>
                         <center> <?php echo $flavor['flavor'] ?> </center>
                     </strong>
+                    <br>
+                    <button onclick="addToCart(<?php echo $flavor['id'] ?>)">Add to Cart</button>
+                    <button onclick="location.href='cart.php'">View Cart</button>
                 </div>
             </main>
         <?php
         }
         ?>
     </div>
+    <div>
+        Cart: <span id="cart-count"><?php echo count($_SESSION['cart']) ?></span> items
+    </div>
 </body>
 
-</html> 
+</html>
